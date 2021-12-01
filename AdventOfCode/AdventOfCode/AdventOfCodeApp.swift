f74cdb7a4ef4946c65857228dc420ade92ab7c64
Mike Ciesielka <maciesielka@gmail.com>
2021-12-01 09:47:38 -0500

    Update project structure to import swift packages for helpers

---
+++ AdventOfCode/AdventOfCode/AdventOfCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeApp.swift
+//  AdventOfCode
+//
+//  Created by Mike Ciesielka on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
